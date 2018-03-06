039a1c1b32ba000f8d788a620f6d31ce3d537846
audrey chae <audreychae97@hotmail.com>
2018-03-06 14:14:31 -0800

    create order javascript file

---
+++ programFiles/src/js/createOrder.js
@@ -0,0 +1,23 @@
+var request = new XMLHttpRequest();
+function shittyMethod(productID){
+    console.log(productID);
+    
+    
+}
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+
+request.onload = function () {
+
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+      console.log(movie.title);
+    });
+  } else {
+    console.log('error');
+  }
+}
+
+request.send();
