6048151a1913223ec1c3c437a2264213c9c934b0
audrey chae <audreychae97@hotmail.com>
2018-03-13 02:14:49 -0700

    reload on inventory

--- programFiles/src/js/inventory.js
+++ programFiles/src/js/inventory.js
@@ -3,10 +3,11 @@ function increment(quantityAmount, productID){
     //doc get element by id
     var address = `http://127.0.0.1:3000/inventory/increment/${quantityAmount}/${productID}`;
     request.open('PUT', address, true);
+    window.location.reload();
 
     // request.onload = function () {
     //     if (request.status >= 200 && request.status < 400) {
-    //         console.log("inside rthe onload"); 
+    //         console.log("inside rthe onload");
     //     } else {
     //      console.log('FUCK');
     //     }
@@ -17,9 +18,10 @@ function decrement(quantityAmount, productID){
     //doc get element by id
     var address = `http://127.0.0.1:3000/inventory/decrement/${quantityAmount}/${productID}`;
     request.open('PUT', address, true);
+    window.location.reload();
     // request.onload = function () {
     //     if (request.status >= 200 && request.status < 400) {
-    //         console.log("inside rthe onload"); 
+    //         console.log("inside rthe onload");
     //     } else {
     //      console.log('FUCK');
     //     }
